7745d10b6e48ab18419c0b772fd873109921285f
marijaparezanin <marija.parezanin@mensa.ba>
2024-11-30 16:37:34 +0100

    [MOD] - Change login and registration navigation so they now use their own navigation graph

--- app/src/main/java/com/ftn/eventhopper/fragments/registration/ConfirmEmailFragment.java
+++ app/src/main/java/com/ftn/eventhopper/fragments/registration/ConfirmEmailFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,8 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.ftn.eventhopper.R;
-import com.ftn.eventhopper.activities.HostActivity;
-import com.ftn.eventhopper.activities.LoginActivity;
+import com.ftn.eventhopper.fragments.LoginFragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,24 +23,16 @@ import com.ftn.eventhopper.activities.LoginActivity;
  */
 public class ConfirmEmailFragment extends Fragment {
 
-    private Button loginBtn;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_confirm_email, container, false);
 
-
-        loginBtn = view.findViewById(R.id.login_btn);
-
-        loginBtn.setOnClickListener(v -> handleBackToHomepage());
+        view.findViewById(R.id.login_btn).setOnClickListener(v -> {
+            NavController navController = NavHostFragment.findNavController(this);
+            navController.navigate(R.id.action_to_login);
+        });
 
         return view;
     }
 
-    private void handleBackToHomepage() {
-        Log.d("Confirm email", "Back to login clicked");
-
-        Intent intent = new Intent(getActivity(), LoginActivity.class);
-        startActivity(intent);
-    }
 }
