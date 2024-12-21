820e737143a370e5ee3093a3e59749d83bab464a
Luka Bradic <bradicluka04@gmail.com>
2024-12-21 15:03:35 +0100

    [ADD] - Add link to backend

---
+++ app/src/main/java/com/ftn/eventhopper/clients/ClientUtils.java
@@ -0,0 +1,37 @@
+package com.ftn.eventhopper.clients;
+
+
+import java.util.concurrent.TimeUnit;
+
+import com.ftn.eventhopper.BuildConfig;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ClientUtils {
+
+    public static final String SERVICE_API_PATH = "http://"+ BuildConfig.IP_ADDR +":8080/api/";
+
+    public static Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(SERVICE_API_PATH)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(setupClient())
+            .build();
+
+    public static OkHttpClient setupClient(){
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(120, TimeUnit.SECONDS)
+                .readTimeout(120, TimeUnit.SECONDS)
+                .writeTimeout(120, TimeUnit.SECONDS)
+                .addInterceptor(interceptor).build();
+
+        return client;
+    }
+
+    //public static ProductService productService = retrofit.create(ProductService.class);
+}
