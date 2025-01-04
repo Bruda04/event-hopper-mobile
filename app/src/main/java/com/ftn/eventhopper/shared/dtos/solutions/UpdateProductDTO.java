8249795cb80d1291e43b532ac506cd76627c7d0d
Luka Bradic <bradicluka04@gmail.com>
2025-01-04 16:47:54 +0100

    [ADD] - Add DTOs

---
+++ app/src/main/java/com/ftn/eventhopper/shared/dtos/solutions/UpdateProductDTO.java
@@ -0,0 +1,20 @@
+package com.ftn.eventhopper.shared.dtos.solutions;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Collection;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UpdateProductDTO {
+    private String name;
+    private String description;
+    private Collection<String> pictures;
+    private boolean isAvailable;
+    private boolean isVisible;
+    private Collection<UUID> eventTypesIds;
+}
