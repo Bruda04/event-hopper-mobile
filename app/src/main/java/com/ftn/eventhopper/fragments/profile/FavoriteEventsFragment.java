15a6a192eacb1b85c009bbe465650fbfcfd85aee
marijaparezanin <marija.parezanin@mensa.ba>
2025-01-08 20:27:55 +0100

    [WIP] - Work on profile navigation

--- app/src/main/java/com/ftn/eventhopper/fragments/profile/FavoriteEventsFragment.java
+++ app/src/main/java/com/ftn/eventhopper/fragments/profile/FavoriteEventsFragment.java
@@ -1,15 +1,13 @@
-package com.ftn.eventhopper.fragments.home;
+package com.ftn.eventhopper.fragments.profile;
 
 import android.os.Bundle;
 
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.ftn.eventhopper.R;
 import com.ftn.eventhopper.adapters.ServiceAdapter;
@@ -18,13 +16,8 @@ import com.github.islamkhsh.CardSliderViewPager;
 
 import java.util.ArrayList;
 
-public class HomeSolutionsFragment extends Fragment {
 
-    private Button filterButton_solutions;
-
-    public HomeSolutionsFragment() {
-        // Required empty public constructor
-    }
+public class FavoriteEventsFragment extends Fragment {
 
     private ArrayList<Service> loadTop5Services() {
         ArrayList<Service> services = new ArrayList<>();
@@ -38,41 +31,27 @@ public class HomeSolutionsFragment extends Fragment {
         return services;
     }
 
+    public FavoriteEventsFragment() {
+        // Required empty public constructor
+    }
+    public static FavoriteEventsFragment newInstance(String param1, String param2) {
+        FavoriteEventsFragment fragment = new FavoriteEventsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home_solutions, container, false);
-
+        View view = inflater.inflate(R.layout.fragment_favorite_events, container, false);
         CardSliderViewPager cardSliderViewPager = view.findViewById(R.id.viewPagerSolutions);
         cardSliderViewPager.setAdapter(new ServiceAdapter(loadTop5Services()));
 
-        filterButton_solutions = view.findViewById(R.id.filterButtonSolution);
-        filterButton_solutions.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
-                SolutionsFilterSort bottomSheet = new SolutionsFilterSort();
-                bottomSheet.show(fragmentManager, "FilterSortBottomSheet");
-            }
-        });
-
         return view;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
     }
 }
