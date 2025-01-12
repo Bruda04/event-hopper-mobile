318db3827309db3b2d23bdbfe495d8acb371143d
Vanja Kostic <vanjakostic03@gmail.com>
2025-01-12 16:28:01 +0100

    [WIP] - redirecting

--- app/src/main/java/com/ftn/eventhopper/activities/InvitationActivity.java
+++ app/src/main/java/com/ftn/eventhopper/activities/InvitationActivity.java
@@ -8,13 +8,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import com.ftn.eventhopper.R;
 import com.ftn.eventhopper.clients.ClientUtils;
 import com.ftn.eventhopper.clients.interceptors.JWTInterceptor;
+import com.ftn.eventhopper.shared.dtos.events.SimpleEventDTO;
+import com.ftn.eventhopper.shared.dtos.invitations.InvitationDTO;
 import com.ftn.eventhopper.shared.dtos.users.account.SimpleAccountDTO;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class InvitationActivity extends AppCompatActivity {
+
+    private MutableLiveData<InvitationDTO> invitationDTO = new MutableLiveData<>();
+    private MutableLiveData<SimpleAccountDTO> accountDTO = new MutableLiveData<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +38,7 @@ public class InvitationActivity extends AppCompatActivity {
         Uri data = getIntent().getData();
         if (data != null) {
             String invitationId = data.getLastPathSegment(); // "6666407d-dc7b-4288-a318-a5be27fbdf7d"
-            //dobaviti pozivnicu i iz nje dobaviti email
-
-            //SimpleAccountDTO dto = ClientUtils.profileService.getActiveByEmail();
+            getInvitationById(UUID.fromString(invitationId));
 
 
             // Proverite korisnika
@@ -35,6 +47,53 @@ public class InvitationActivity extends AppCompatActivity {
 //            } else {
 //                redirectToLoginOrRegistration(invitationId);
 //            }
+        }
+    }
+
+    private void getInvitationById(UUID id) {
+
+        Call<InvitationDTO> call = ClientUtils.invitationService.getInvitation(id);
+        call.enqueue(new Callback<InvitationDTO>() {
+
+            @Override
+            public void onResponse(Call<InvitationDTO> call, Response<InvitationDTO> response) {
+                if(response.isSuccessful()){
+                    invitationDTO.postValue(response.body());
+                    getAccountByEmail(invitationDTO.getValue().getTargetEmail());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<InvitationDTO> call, Throwable t) {
+
+            }
+        });
+
+    }
+
+    private void getAccountByEmail(String email) {
+
+        Call<SimpleAccountDTO> call = ClientUtils.profileService.getActiveByEmail(email);
+        call.enqueue(new Callback<SimpleAccountDTO>() {
+
+            @Override
+            public void onResponse(Call<SimpleAccountDTO> call, Response<SimpleAccountDTO> response) {
+                if(response.isSuccessful()){
+                    accountDTO.postValue(response.body());
+                    redirect();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<SimpleAccountDTO> call, Throwable t) {
+
+            }
+        });
+    }
+
+    public void redirect(){g
+        if(accountDTO == null){
+            //redirektuj na registraciju
         }
     }
 
--- app/src/main/java/com/ftn/eventhopper/clients/services/invitations/InvitationService.java
+++ app/src/main/java/com/ftn/eventhopper/clients/services/invitations/InvitationService.java
@@ -1,11 +1,16 @@
 package com.ftn.eventhopper.clients.services.invitations;
 
 import com.ftn.eventhopper.shared.dtos.invitations.CreateInvitationDTO;
+import com.ftn.eventhopper.shared.dtos.invitations.InvitationDTO;
+
+import java.util.UUID;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface InvitationService {
 
@@ -15,5 +20,12 @@ public interface InvitationService {
     })
     @POST("invitations")
     Call<Void> create(@Body CreateInvitationDTO invitationDTO);
+
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type:application/json"
+    })
+    @GET("invitations/{id}")
+    Call<InvitationDTO> getInvitation(@Path("id") UUID id);
 
 }
