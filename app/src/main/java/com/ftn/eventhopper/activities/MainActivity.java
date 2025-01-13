34cb01d5b6edb4686593eb97060e371c3995f2d3
marijaparezanin <marija.parezanin@mensa.ba>
2025-01-13 12:59:09 +0100

    [ADD] - Add checking if user already logged in

--- app/src/main/java/com/ftn/eventhopper/activities/MainActivity.java
+++ app/src/main/java/com/ftn/eventhopper/activities/MainActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.NavGraph;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.ftn.eventhopper.R;
 import com.ftn.eventhopper.clients.services.auth.UserService;
+import com.ftn.eventhopper.fragments.HostFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,17 +21,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         UserService.initialize(getApplicationContext());
 
-//        UserService.clearJwtToken();
-//        UserService.setJwtToken("eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJldmVudC1ob3BwZXIiLCJzdWIiOiJwdXAzQGV4YW1wbGUuY29tIiwiYXVkIjoid2ViIiwiaWF0IjoxNzM1NTEzMjg5LCJleHAiOjE3MzU1MjQwODksInJvbGUiOiJTRVJWSUNFX1BST1ZJREVSIiwiaWQiOiIzMTU0OGQ2Yi0wMTlmLTQ5MmUtYmEzOS0wN2JlN2ExNDMzZTUifQ.kxHVlTLpNfG8yha3kMf4yVPVTKGVFGJohI6vDLtpcup_vn8IL69tj-Zsst9Yyjs_WP0aqlGsESMzbYo745nZMw");
-
-
-    Log.d("MainActivity", "setContentView called");
-
+        Log.d("MainActivity", "setContentView called");
         // Correctly referencing the FragmentContainerView ID
+
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.nav_main_fragment);
+            .findFragmentById(R.id.nav_main_fragment);
 
         NavController navController = navHostFragment.getNavController();
+        Fragment fragment;
+        if(UserService.isTokenValid()){
+            fragment = new HostFragment();
+        }else{
+            fragment = NavHostFragment.create(R.navigation.nav_auth);
+        }
 
+        Log.d("TOKEN HERE", String.valueOf(UserService.isTokenValid()));
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.nav_main_fragment, fragment)
+                .commit();
     }
 }
