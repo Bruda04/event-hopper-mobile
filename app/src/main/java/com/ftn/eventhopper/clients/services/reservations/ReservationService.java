323779617c0bf6238215b894b1f5e6d2110ea085
Luka Bradic <bradicluka04@gmail.com>
2025-02-19 13:57:13 +0100

    [FIX] - Fix wrong HTTP method

--- app/src/main/java/com/ftn/eventhopper/clients/services/reservations/ReservationService.java
+++ app/src/main/java/com/ftn/eventhopper/clients/services/reservations/ReservationService.java
@@ -7,14 +7,14 @@ import com.ftn.eventhopper.shared.dtos.reservations.CreatedReservationProductDTO
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.POST;
 
 public interface ReservationService {
     @Headers({
             "User-Agent: Mobile-Android",
             "Content-Type:application/json"
     })
-    @GET("reservations/products")
+    @POST("reservations/products")
     Call<ResponseBody> buyProduct(@Body CreateReservationProductDTO createReservationProductDTO);
 }
